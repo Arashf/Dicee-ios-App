8bdf7f18c5272084ec9e1803be10c2e8bd1fc44e
Arash Farahmandi <farahmandi.arash@gmail.com>
2018-12-11 21:16:53 +0330

    Project Created

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Arash Farahmandi on 9/20/1397 AP.
+//  Copyright © 1397 1To100Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
